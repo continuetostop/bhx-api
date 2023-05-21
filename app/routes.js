bc1234d713cf3752f945e75a8d75588556d754c2
continuetostop <phuc.ml.dl@gmail.com>
2023-05-21 19:49:15 +0700

    update auth

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
   require('./routes/UnitRoute')(app);
   require('./routes/GroupUnitRoute')(app);
   require('./routes/UnitConversionRoute')(app);
-  require('./routes/AuthRoute')(app);
+  // require('./routes/AuthRoute')(app);
   require('./routes/OrderRoute')(app);
 };
